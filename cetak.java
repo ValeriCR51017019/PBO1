91652d7915ed8fbda981fd5485183ad36cabbd13
ValeriCR51017019 <48231242+ValeriCR51017019@users.noreply.github.com>
2019-09-12 12:17:25 +0800

    Add files via upload

---
+++ cetak.java
@@ -0,0 +1,15 @@
+/**
+ * @(#)cetak.java
+ *
+ *
+ * @author 
+ * @version 1.00 2019/9/12
+ */
+
+
+public class cetak {
+
+    public static void main(String[] args){
+    	System.out.print("valeri \n" + "sangat \n" + "tampan");
+    }
+}
